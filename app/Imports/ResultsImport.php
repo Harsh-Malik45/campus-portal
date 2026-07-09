c23c8f092eda0397a822ba85c9931ef206a1a1b9
Harsh-Malik45 <malikharsh.0045@gmail.com>
2026-07-09 12:30:48 +0530

    Implement Excel import for student results

--- app/Imports/ResultsImport.php
+++ app/Imports/ResultsImport.php
@@ -3,19 +3,39 @@
 namespace App\Imports;
 
 use App\Models\Result;
-use Maatwebsite\Excel\Concerns\ToModel;
+use App\Models\Student;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ResultsImport implements ToModel
+class ResultsImport implements ToCollection, WithHeadingRow
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new Result([
-            //
-        ]);
+        foreach ($rows as $row) {
+
+            $student = Student::where('roll_no', $row['roll_no'])->first();
+
+            if ($student) {
+
+               Result::updateOrCreate(
+
+    [
+        'student_id' => $student->id,
+        'subject' => $row['subject'],
+    ],
+
+    [
+        'max_marks' => $row['max_marks'],
+        'obtained_marks' => $row['obtained_marks'],
+    ]
+
+);
+
+                
+
+            }
+
+        }
     }
 }
